61b229d1c5c04633bec01979c1d74473827a0415
mohamed amin <mohamedamin0az@gmail.com>
2024-02-18 16:43:35 +0200

    change openAi key

--- src/pages/dashBoard.jsx
+++ src/pages/dashBoard.jsx
@@ -2,12 +2,30 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 const DashBoard = () => {
   const [elevenKey, setElevenKey] = useState("");
+  const [openAIKey, setOpenAIKey] = useState("");
   const navigate = useNavigate();
   const [selectedOption, setSelectedOption] = useState("");
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
     localStorage.setItem("gender", event.target.value);
+  };
+  const handleOpenAiKey = () => {
+    fetch("http://localhost:8000/admin/config", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        openAiKey: openAIKey,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setOpenAIKey("");
+      })
+      .catch((error) => console.error("Error:", error));
   };
 
   return (
@@ -17,7 +35,7 @@ const DashBoard = () => {
         onChange={(e) => {
           setElevenKey(e.target.value);
         }}
-        placeholder="Enter key "
+        placeholder="Enter new eleven key "
         style={{ height: "30px" }}
       ></input>
       <button
@@ -31,7 +49,26 @@ const DashBoard = () => {
           cursor: "pointer",
         }}
       >
-        add new speech key
+        change Eleven key
+      </button>
+      <br></br>
+      <input
+        onChange={(e) => {
+          setOpenAIKey(e.target.value);
+        }}
+        placeholder="Enter new open ai key "
+        style={{ height: "30px", marginTop: "30px" }}
+      ></input>
+      <button
+        onClick={handleOpenAiKey}
+        style={{
+          padding: "7px",
+          marginLeft: "15px",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        change openAi key
       </button>
       <div style={{ marginTop: "70px" }}>
         <label htmlFor="dropdown" style={{ marginRight: "10px" }}>
